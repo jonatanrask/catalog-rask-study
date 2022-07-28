d7cef61d3d0ae6c5726f1e68b97c6578b2091958
Jonatan Rask <jonatan16.veloz@gmail.com>
2022-07-28 19:53:52 -0400

    H2 database

---
+++ backend/src/main/java/com/rask/catalog/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.rask.catalog.repositories;
+
+import com.rask.catalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
